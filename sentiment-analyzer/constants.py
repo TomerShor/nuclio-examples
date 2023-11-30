add1ab45d9f089d6a40740622f335d424391858d
TomerShor <tomer.shor@gmail.com>
2023-11-30 17:10:31 +0200

    Sentiment Analyzer

---
+++ sentiment-analyzer/constants.py
@@ -0,0 +1,8 @@
+class Constants:
+    SENTIMENT_MAPPING = {
+        0: "very negative ",
+        1: "negative",
+        2: "neutral",
+        3: "positive",
+        4: "very positive"
+    }
---
+++ sentiment-analyzer/preprocessing.py
@@ -0,0 +1,9 @@
+class Tokenizer(object):
+    def __init__(self, tokenizer_function):
+        self.tokenizer_function = tokenizer_function
+
+    def tokenize(self, text, **kwargs):
+        return self.tokenizer_function(
+            text,
+            **kwargs
+        )
