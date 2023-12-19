67e0516605dcdbe3d640aa1f53fb51a18b963c89
TomerShor <tomer.shor@gmail.com>
2023-12-19 18:03:30 +0200

    Improve sentiment analyzer

--- sentiment_analyzer/sentiment_analyzer.py
+++ sentiment_analyzer/sentiment_analyzer.py
@@ -5,22 +5,28 @@ The function receives a text input and returns the predicted sentiment of the te
 """
 
 import os
-import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 import preprocessing
-from common.constants import SENTIMENT_MAPPING
+from common.constants import SentimentMapping
 
 
 def init_context(context):
     # load the model here, just once
     if not getattr(context.user_data, "initialized", False):
+        # get model name from environment variable
         model_name = os.getenv("MODEL_NAME", "nlptown/bert-base-multilingual-uncased-sentiment")
+
+        context.logger.info(f"Initializing the model. Model name: {model_name}")
         model = AutoModelForSequenceClassification.from_pretrained(model_name)
         tokenizer = preprocessing.Tokenizer(AutoTokenizer.from_pretrained(model_name))
+
+        # save the model and tokenizer in the context user data, to be shared between invocations
         setattr(context.user_data, "model", model)
         setattr(context.user_data, "tokenizer", tokenizer)
         setattr(context.user_data, "initialized", True)
+
+        context.logger.info("Model initialized")
 
 
 def handler(context, event):
@@ -29,8 +35,14 @@ def handler(context, event):
 
     context.logger.info(f"Received input text: {input_text}")
 
-    # call the sentiment function
-    predicted_sentiment = _predict_text_sentiment(context.user_data.tokenizer, context.user_data.model, input_text)
+    # call the sentiment prediction function
+    predicted_sentiment = _predict_text_sentiment(
+        context.user_data.tokenizer,
+        context.user_data.model,
+        input_text,
+    )
+
+    context.logger.info(f"Predicted sentiment: {predicted_sentiment}")
 
     # return the sentiment
     body = {
@@ -48,20 +60,21 @@ def handler(context, event):
 
 
 def _predict_text_sentiment(tokenizer, sentiment_model, input_text):
+    # Tokenize the input text
     inputs = tokenizer.tokenize(
         input_text,
         truncation=True,
         padding=True,
         return_tensors="pt"
     )
-    sentiment_model.eval()
-    with torch.no_grad():
-        outputs = sentiment_model(**inputs)
 
-    # Get the classification score for each token and pick the highest one
-    logits = outputs.logits
-    predicted_class_sentiment = torch.argmax(logits, dim=1).item()
+    # Run the text through the sentiment model
+    sentiment_model.eval()
+    outputs = sentiment_model(**inputs)
+
+    # Get the classification
+    predicted_class_sentiment = outputs.logits.argmax(dim=1).item()
 
     # Map the sentiment label to a rating
-    predicted_label_sentiment = SENTIMENT_MAPPING[predicted_class_sentiment]
+    predicted_label_sentiment = SentimentMapping.to_str(predicted_class_sentiment)
     return predicted_label_sentiment
